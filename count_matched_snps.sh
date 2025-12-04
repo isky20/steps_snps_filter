fcc408bea6a6b2dc8ba392ae53781a346c3d6966
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2025-12-04 17:26:47 +0300

    Update count_matched_snps.sh

--- count_matched_snps.sh
+++ count_matched_snps.sh
@@ -1,4 +1,4 @@
-# Easy way with plink2 
+# with plink2 
 plink2 \
   --pfile snps_array_step7_noambig \
   --extract wgs_ps_step1.snplist \
@@ -7,3 +7,31 @@ plink2 \
 
 wc -l snps_array_wgs_overlap.snplist
 
+# Array-only SNPs = all array SNPs minus overlap
+plink2 \
+  --pfile snps_array_step7_noambig \
+  --exclude snps_array_wgs_overlap.snplist \
+  --write-snplist \
+  --out snps_array_only_not_in_wgs
+
+  wc -l snps_array_only_not_in_wgs
+
+
+# All SNPs from WGS
+plink2 \
+  --pfile wgs_ps_step1 \
+  --write-snplist \
+  --out wgs_all
+
+wc -l  wgs_all
+
+
+
+# WGS-only SNPs = all WGS SNPs minus overlap
+plink2 \
+  --pfile wgs_ps_step1 \
+  --exclude snps_array_wgs_overlap.snplist \
+  --write-snplist \
+  --out wgs_only_not_in_array
+
+wc -l  wgs_only_not_in_array
