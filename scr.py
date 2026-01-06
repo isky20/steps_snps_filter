d5470cf4826f13d2fbdf5ee2dcc9de41ff47600b
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2026-01-06 11:26:59 +0300

    Refine SNPs data selection comments
    Formatted the SNPs data selection criteria for clarity.

--- scr.py
+++ scr.py
@@ -1,13 +1,15 @@
 #SNPs Data Selection
 
-###
+"""
+SNPs Data Selection
 Standard GWAS QC:
- Genotyping rate >0,99
- Remove SNPs with >5% missing data
- Heterozygosity. Remove samples > mean(proportion of heterozygous sites ) ± 3 * sd(proportion of heterozygous sites)
- Exclude individuals with sex discordance
- Imputation information score (INFO score). > 0.3 – 0.8
- Minor Allele Frequency (MAF). > 0.005 – 0,001
- Linkage-disequilibrium (LD) score
- Hardy-Weinberg equilibrium (HWE) P>1x10-6
-###
+  - Genotyping rate > 0.99
+  - Remove SNPs with > 5% missing data
+  - Heterozygosity: remove samples > mean(proportion of heterozygous sites)
+    ± 3 * sd(proportion of heterozygous sites)
+  - Exclude individuals with sex discordance
+  - INFO score > 0.3–0.8
+  - MAF > 0.005–0.001
+  - LD score
+  - HWE P > 1e-6
+"""
