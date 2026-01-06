0842f9ff804c987a5c96d4d8be01a040b656add5
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2026-01-06 11:33:09 +0300

    Update scr.py

--- scr.py
+++ scr.py
@@ -3,8 +3,8 @@
 """
 SNPs Data Selection
 Standard GWAS QC:
-  - Genotyping rate > 0.99
-  - Remove SNPs with > 5% missing data
+  - Genotyping rate > 0.90
+  - Remove SNPs with > 10% missing data
   - Heterozygosity: remove samples > mean(proportion of heterozygous sites)
     ± 3 * sd(proportion of heterozygous sites)
   - Exclude individuals with sex discordance
@@ -12,4 +12,22 @@ Standard GWAS QC:
   - MAF > 0.005–0.001
   - LD score
   - HWE P > 1e-6
+"""
+
+#Target Data
+"""
+* More than 100 individuals
+* Genome build
+* No sample overlap with SNPs GWAS samples
+
+Standard GWAS For samples:
+  -  Individuals genotype rate >0.99
+  -  Remove sample with >10% missing data
+  -  Heterozygosity. Remove samples > mean(proportion of heterozygous sites ) ± 3 * sd(proportion of heterozygous sites)
+  -  Exclude individuals with sex discordance
+  -  Remove duplicates and relatedness samples. PI_HAT > 0.8 For SNPs:
+  -  Imputation information score (INFO score). > 0.3 – 0.8
+  -  Minor Allele Frequency (MAF). > 0.01 - 0.005
+  -  Linkage-disequilibrium (LD) score
+  -  Hardy-Weinberg Equilibrium (HWE) p > 1x10-6
 """
